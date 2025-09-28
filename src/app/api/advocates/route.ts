f9b3d964d8fa1d94c5f71e4904e6c02c6265b0a3
JEB <jebenois@gmail.com>
2025-09-28 18:37:40 +0200

    Fix UX anti patterns: Missing loading/error states
    I noticed it was missing some basic error handling. It was a great opportunity to also fix the missing loading/error states which could be seen as a glaring UX anti-pattern.
    
    - Added error handling on the server side.
    - Added error handling on the client side.
    - Added loading/error states to the page.

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,8 +1,15 @@
-import db from "../../../db";
-import { advocates } from "../../../db/schema";
+import db from "@/db";
+import { advocates } from "@/db/schema";
 
 export async function GET() {
-  const data = await db.select().from(advocates);
+  try {
+    const data = await db.select().from(advocates);
 
-  return Response.json({ data });
+    return Response.json({ data });
+  } catch (error) {
+    // TODO:Log the error to Sentry etc.
+    console.error("Error fetching advocates:", error);
+
+    return Response.json({}, { status: 500 });
+  }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,19 +7,38 @@ export default function Home() {
   const [advocates, setAdvocates] = useState<Advocate[]>([]);
   const [filteredAdvocates, setFilteredAdvocates] = useState<Advocate[]>([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    fetch("/api/advocates").then((response) => {
-      response.json().then((jsonResponse) => {
+    const fetchAdvocates = async () => {
+      try {
+        setIsLoading(true);
+        setError(null);
+
+        const response = await fetch("/api/advocates");
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const jsonResponse = await response.json();
+
         setAdvocates(jsonResponse.data);
         setFilteredAdvocates(jsonResponse.data);
-      });
-    });
+      } catch (error) {
+        // Error is recorded on the server side. We don't need to do more here for now.
+        setError("Failed to fetch advocates.");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchAdvocates();
   }, []);
 
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const searchTerm = event.target.value;
-
     setSearchTerm(searchTerm);
 
     const filteredAdvocates = advocates.filter((advocate) => {
@@ -67,38 +86,44 @@ export default function Home() {
       </div>
       <br />
       <br />
-      <table>
-        <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>City</th>
-            <th>Degree</th>
-            <th>Specialties</th>
-            <th>Years of Experience</th>
-            <th>Phone Number</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredAdvocates.map((advocate) => {
-            return (
-              <tr key={advocate.id}>
-                <td>{advocate.firstName}</td>
-                <td>{advocate.lastName}</td>
-                <td>{advocate.city}</td>
-                <td>{advocate.degree}</td>
-                <td>
-                  {advocate.specialties.map((specialty) => (
-                    <div key={specialty}>{specialty}</div>
-                  ))}
-                </td>
-                <td>{advocate.yearsOfExperience}</td>
-                <td>{advocate.phoneNumber}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+
+      {isLoading && <p>Loading...</p>}
+      {!isLoading && error && <p>{error}</p>}
+
+      {!isLoading && !error && (
+        <table>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>City</th>
+              <th>Degree</th>
+              <th>Specialties</th>
+              <th>Years of Experience</th>
+              <th>Phone Number</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredAdvocates.map((advocate) => {
+              return (
+                <tr key={advocate.id}>
+                  <td>{advocate.firstName}</td>
+                  <td>{advocate.lastName}</td>
+                  <td>{advocate.city}</td>
+                  <td>{advocate.degree}</td>
+                  <td>
+                    {advocate.specialties.map((specialty) => (
+                      <div key={specialty}>{specialty}</div>
+                    ))}
+                  </td>
+                  <td>{advocate.yearsOfExperience}</td>
+                  <td>{advocate.phoneNumber}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </main>
   );
 }
